7512dd36f70886e77aa97096c7fe27e244069648
Lebugg <lebugg@hotmail.com>
2013-12-11 23:25:40 +0100

    Comment of DateTime class.

--- lib/DateTime.php
+++ lib/DateTime.php
@@ -1,20 +1,49 @@
 <?php
 namespace SimpleAR;
+/**
+ * This file contains a extension of PHP DateTime class.
+ */
 
 /**
- * Thanks to gmblar+php at gmail dot com ¶
+ * This extension implements __toString() function to automatically format DateTime when
+ * using instance as a string.
+ *
+ * In combination, we use a member variable that holds the date format to use. This property at
+ * initialization of SimpleAR so that it can be changed if application internationalized.
+ *
+ * Thanks to gmblar+php at gmail dot com
  * at http://www.php.net/manual/fr/class.datetime.php#95830
  * for this DateTime extension.
  */
 class DateTime extends \DateTime
 {
+    /**
+     * Date format to use when trying to use DateTime instance as string.
+     *
+     * @var string Default: 'Y-m-d';
+     */
     private static $_sFormat = 'Y-m-d';
 
+    /**
+     * PHP magical __toString method.
+     *
+     * When the DateTime will be used as a string, this function will be magically fired and it will
+     * automatically format the DateTime to the format specified by `$_sFormat` property.
+     *
+     * @return string
+     *
+     * @see DateTime::$_sFormat.
+     */
     public function __toString()
     {
         return $this->format(self::$_sFormat);
     }
 
+    /**
+     * Date format setter.
+     *
+     * @param string $s The date format to use.
+     */
     public static function setFormat($s)
     {
         self::$_sFormat = $s;
