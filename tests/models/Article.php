be52a7e96758c2fa33e96bf276cf7336b23a9c5d
Lebugg <lebugg@outlook.com>
2014-07-28 10:32:04 +0200

    Remove models/ dir

--- tests/models/Article.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Article extends SimpleAR\Orm\Model
-{
-    public static function to_conditions_relevant($attribute)
-    {
-        return array(array('content', 'LIKE', '%pokemon%'));
-    }
-}
